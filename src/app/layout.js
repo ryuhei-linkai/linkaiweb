0d3cf5c976146434c4e6751dfecb45af1773059b
ryuhei-linkai <ryuhei.ando@linkai.co.jp>
2024-10-04 16:21:12 +0900

    ヒートマップ

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,17 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="icon" href="/images/ファビコン.png" />
+        {/* Begin Mieruca Embed Code */}
+        <script type="text/javascript" id="mierucajs" dangerouslySetInnerHTML={{
+          __html: `
+            window.fid = window.fid || [];fid.push([873070815]);
+            (function() {
+              function mieruca(){if(typeof window.fjsld != "undefined") return; window.__fjsld = 1; var fjs = document.createElement('script'); fjs.type = 'text/javascript'; fjs.async = true; fjs.id = "fjssync"; var timestamp = new Date;fjs.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://hm.mieru-ca.com/service/js/mieruca-hm.js?v='+ timestamp.getTime(); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(fjs, x); };
+              setTimeout(mieruca, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent("onload", mieruca) : window.addEventListener("load", mieruca, false)) : mieruca();
+            })();
+          `
+        }} />
+        {/* End Mieruca Embed Code */}
       </head>
       <body className={`${inter.className} ${notoSansJP.className}`}>{children}</body>
     </html>
